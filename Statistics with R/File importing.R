87a33f9a1489e04a82233dbf289c86db5a148934
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-10 22:39:30 -0300

    Import CSV file and change variable types

--- Statistics with R/File importing.R
+++ Statistics with R/File importing.R
@@ -1,3 +1,16 @@
 # remover todos os objetos
 rm(list=ls())
 
+library(readr)
+cadastro <- read_csv("fiap-mba-dts\\Statistics with R\\Paperwork\\Aula01\\cadastro.csv")
+View(cadastro)
+str(cadastro)
+
+# Analise
+summary(cadastro)
+
+# Mudar o formato da variavel quantitativa para qualitativa
+cadastro$CEP <- factor(cadastro$CEP)
+
+str(cadastro$CEP)
+summary(cadastro$CEP)
