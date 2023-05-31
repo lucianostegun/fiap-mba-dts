1f099dec9a0de5def0b24dc71a1f06a948ce0408
Luciano Stegun <lucianostegun@gmail.com>
2023-05-31 19:34:39 -0300

    Update of exercise 6 of class 3

--- Statistics with R/Paperwork/Class 03/Trab3.R
+++ Statistics with R/Paperwork/Class 03/Trab3.R
@@ -1,9 +1,13 @@
 library(readxl)
-sal_lab1 <- read_excel("Documents/fiap-mba-dts/Statistics with R/Paperwork/Class 03/sal_lab1.xlsx")
+sal_lab1 <- read_excel("fiap-mba-dts/Statistics with R/Paperwork/Class 03/sal_lab1.xlsx")
 View(sal_lab1)
 sal_lab1$Zscore
 
-hist(sal_lab1$Ingestao_Sal)
-zscore = (sal_lab1$Ingestao_Sal - mean(sal_lab1$Ingestao_Sal)) / sd(sal_lab1$Ingestao_Sal)
-hist(zscore)
+hist(sal_lab1$Ingestao_Sal, main = "Distribuição da ingestão de sal (g/dia)", ylab = "Fraquência", xlab="Ingestão de sal (g/dia)")
+
+media = mean(sal_lab1$Ingestao_Sal)
+desvio_padrao = sd(sal_lab1$Ingestao_Sal)
+sal_lab1$zscore = (sal_lab1$Ingestao_Sal - media) / desvio_padrao
+hist(sal_lab1$zscore)
+hist(zscore, main = "Distribuição da ingestão de sal (g/dia)", ylab = "Fraquência", xlab="Ingestão de sal (g/dia)")
 
