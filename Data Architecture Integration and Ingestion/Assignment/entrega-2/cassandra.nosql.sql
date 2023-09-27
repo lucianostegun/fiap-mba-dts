190eb94586780ef5360c137de63e5a9024ff648c
Luciano Stegun <lucianostegun@gmail.com>
2023-09-26 21:29:24 -0300

    Folder restructuring

---
+++ Data Architecture Integration and Ingestion/Assignment/entrega-2/cassandra.nosql.sql
@@ -0,0 +1,36 @@
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto A', 'Modelo X', 'Fabricante XA1', {'Vermelho', 'Azul'}, {'Pequeno', 'Médio', 'Grande'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto B', 'Modelo Y', 'Fabricante YB2', {'Amarelo', 'Preto'}, {'Extra Grande', '2', '4'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto C', 'Modelo Z', 'Fabricante ZC3', {'Branco', 'Prata', 'Dourado'}, {'6', '8', '10'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto D', 'Modelo X', 'Fabricante XD1', {'Roxo', 'Rosa'}, {'12'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto E', 'Modelo Y', 'Fabricante YE2', {'Vermelho'}, {'Pequeno', 'Médio'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto F', 'Modelo Z', 'Fabricante ZF3', {'Azul', 'Verde'}, {'Grande'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto G', 'Modelo X', 'Fabricante XG1', {'Amarelo'}, {'Extra Grande'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto H', 'Modelo Y', 'Fabricante YH2', {'Preto'}, {'2'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto I', 'Modelo Z', 'Fabricante ZI3', {'Branco', 'Prata'}, {'4'});
+INSERT INTO produtos (codigo, nome, modelo, fabricante, cor, tam) VALUES
+(uuid(), 'Produto J', 'Modelo X', 'Fabricante XJ1', {'Dourado', 'Roxo'}, {'8'});
+
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('12345678901', 'João Silva', 'Rua das Flores, 123', '12345678', 'joao.silva@email.com', {'11928374651'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('23456789012', 'Ana Santos', 'Avenida Principal, 456', '23456789', 'ana.santos@email.com', {'11929363947'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('34567890123', 'Luiz Ferreira', 'Travessa da Praia, 789', '34567890', 'luiz.ferreira@email.com', {'11999283664'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('45678901234', 'Marina Oliveira', 'Alameda dos Sonhos, 101', '45678901', 'marina.oliveira@email.com', {'11928837640'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('56789012345', 'Carlos Souza', 'Rua do Comércio, 55', '56789012', 'carlos.souza@email.com', {'11989475610'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('67890123456', 'Lucia Pereira', 'Avenida Central, 234', '67890123', 'lucia.pereira@email.com', {'11974912203'});
+INSERT INTO clientes (cpf, nome, endereco, cep, email, telefones) VALUES
+('78901234567', 'Eduardo Rodrigues', 'Travessa da Montanha, 12', '78901234', 'eduardo.rodrigues@email.com', {'11999887765'});
+INSERT INTO clientes (cpf, nome
