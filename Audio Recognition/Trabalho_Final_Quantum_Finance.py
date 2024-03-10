1ede5caf2857b0f750f62987b414ddc78bc2012e
Luciano Stegun <lucianostegun@gmail.com>
2024-03-10 15:43:17 -0300

    Audio Recognition - Quantum Finance project

---
+++ Audio Recognition/Trabalho_Final_Quantum_Finance.py
@@ -0,0 +1,7 @@
+from IPython.display import Audio, display
+
+def QuantumFinanceUra():
+  Audio(filename="saudacao.mp3", autoplay=True)
+  Audio(filename="opcoes.mp3", autoplay=True)
+
+QuantumFinanceUra()
